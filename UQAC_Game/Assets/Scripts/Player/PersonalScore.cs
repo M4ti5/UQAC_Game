72dd0320fba3e67205a4a80f3b4c8475992af65c
Botrel Adrien <a7botrel@gmail.com>
2021-10-25 17:52:15 -0400

    Design score personnel (2 versions)
    Voir quel version est la mieux à utiliser

---
+++ UQAC_Game/Assets/Scripts/Player/PersonalScore.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PersonalScore : MonoBehaviour
+{
+    public Image personalScore;
+    public GameObject mask;
+    private int personalScoreMax;
+    private int currentPersonalScore;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        personalScoreMax = 100;
+        currentPersonalScore = 0;
+        ModifyDisplay();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            IncreaseGlobalScore(20);
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            DecreaseGlobalScore(10);
+        }
+    }
+
+    private void IncreaseGlobalScore(int score)
+    {
+        currentPersonalScore += score;
+        if (currentPersonalScore >= personalScoreMax)
+        {
+            currentPersonalScore = personalScoreMax;
+        }
+        ModifyDisplay();
+    }
+
+    private void DecreaseGlobalScore(int score)
+    {
+        currentPersonalScore -= score;
+        if (currentPersonalScore < 0)
+        {
+            currentPersonalScore = 0;
+        }
+        ModifyDisplay();
+    }
+
+    private void ModifyDisplay()
+    {
+        //modifie l'avancement de la barre de vie ainsi que le texte correspondant
+        personalScore.transform.position = mask.transform.position + new Vector3(currentPersonalScore * 300 / personalScoreMax - 300, 0, 0);
+
+    }
+}
