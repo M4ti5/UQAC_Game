ada6fe74427c6d740c7fe9090d3abd379df7b636
Paul <paul.delarue@student.junia.com>
2021-11-09 17:45:37 -0500

    Add view to know when to take object
    When a player can reach an object, a window appears with "take <name of object> press E" written in it
    works in multiplayer

---
+++ UQAC_Game/Assets/Scripts/Player/PlayerStatManager.cs
@@ -0,0 +1,93 @@
+using System.Collections.Generic;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+
+public class PlayerStatManager : MonoBehaviourPun
+{
+    public int currHP = 100;
+    public GameObject allObjects;
+    public GameObject display;
+    public TextMeshProUGUI text;
+
+    public float distanceToHold = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        allObjects = GameObject.Find("Objects");
+        display = GameObject.Find("TakeObject");
+        text = display.transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        List<(GameObject, float)> _reachableObjects = reachableObjects();
+        GameObject nearestObj = findNearestObj(_reachableObjects);
+        if (GetComponent<PhotonView>().IsMine)
+        {
+            if (_reachableObjects.Count > 0)
+            {
+                display.SetActive(true);
+                text.text = "Take " + nearestObj.name + "\nPress E";
+            }
+            else
+            {
+                display.SetActive(false);
+            }
+        }
+        
+    }
+
+    List<(GameObject, float)> reachableObjects()
+    {
+        List<(GameObject, float)> _reachableObjects = new List<(GameObject, float)>();
+        int allObjectCount = allObjects.transform.childCount;
+        for (int i = 0; i < allObjectCount; i++)
+        {
+            (bool _isReachable, float _dist) =
+                IsReachable(allObjects.transform.GetChild(i), gameObject.transform, distanceToHold);
+            if (_isReachable)
+            {
+                _reachableObjects.Add((allObjects.transform.GetChild(i).gameObject,_dist));
+            }
+        }
+
+        return _reachableObjects;
+    }
+    
+    (bool, float) IsReachable(Transform objectA, Transform playerA, float range)
+    {
+        float dist = Vector3.Distance(objectA.position, playerA.position);
+        float angle = Vector3.Angle(playerA.forward, objectA.position - playerA.position);
+    
+        if (dist < range && angle <= Mathf.Abs(30))
+        {
+            return (true, dist);
+        }
+        else
+        {
+            return (false, dist);
+        }
+    
+    }
+
+    GameObject findNearestObj(List<(GameObject, float)> _reachableObjects)
+    {
+        float nearestObjDist = -1;
+        GameObject nearestObj = null;
+        foreach ((GameObject o, float dist) in _reachableObjects)
+        {
+            //first obj
+            if (nearestObjDist == -1 || dist < nearestObjDist)
+            {
+                nearestObj = o;
+                nearestObjDist = dist;
+            }
+            
+        }
+
+        return nearestObj;
+    }
+    
+}
