a118a6967a4e18f611ec14597d1b4e72104b31a2
AntTBD <antbd@free.fr>
2021-11-16 16:56:25 -0500

    upgrade aide with prefab (in UI folder)
    - si le texte dépasse la zonne allouée, on a une scroll bar
    - le script a était mis dans le gameobject parent (c'est là ou on peut personnaliser le texte)

--- UQAC_Game/Assets/Scripts/Player/MessageAideMain.cs
+++ UQAC_Game/Assets/Scripts/Player/MessageAideMain.cs
@@ -1,21 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MessageAideMain : MonoBehaviour
 {
-    public GameObject Texte;
-    public GameObject Aide;
+    // gameobject global avec un background + text + btn fermeture
+    public GameObject panelAide; 
+    
+    // Component de text pour modifier le texte au démarrage
+    public TextMeshProUGUI texteGameObject;
+    
+    // btn principal pour afficher l'aide
+    public GameObject aideBtn;
+    
+    // zone de texte qui sera copié dans le component text
+    [TextArea(3,50)]
+    public string texte;
+
+    private void Start()
+    {
+        // on affecte le texte au component
+        texteGameObject.text = texte;
+        // on s'assure que l'aide est tjrs cachée au démarrage
+        Cacher();
+    }
 
     public void Afficher()
     {
-        Aide.gameObject.SetActive(false);
-        Texte.gameObject.SetActive(true);
+        aideBtn.gameObject.SetActive(false);
+        panelAide.gameObject.SetActive(true);
     }
 
     public void Cacher()
     {
-        Aide.gameObject.SetActive(true);
-        Texte.gameObject.SetActive(false);
+        aideBtn.gameObject.SetActive(true);
+        panelAide.gameObject.SetActive(false);
     }
 }
