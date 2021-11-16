a5dddb68bd6caca9dc3feb71f873ea1e7abc6de9
Amistocle0 <titoinedezedu0304@gmail.com>
2021-11-16 09:53:24 -0500

    Ajout messages d'aide

---
+++ UQAC_Game/Assets/Scripts/MiniGames/MiniGame1/MessageAide.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageAide : MonoBehaviour
+{
+    public GameObject Texte;
+    public GameObject Aide;
+
+    public void Afficher()
+    {
+        Aide.gameObject.SetActive(false);
+        Texte.gameObject.SetActive(true);
+    }
+
+    public void Cacher()
+    {
+        Aide.gameObject.SetActive(true);
+        Texte.gameObject.SetActive(false);
+    }
+}
---
+++ UQAC_Game/Assets/Scripts/Player/MessageAideMain.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageAideMain : MonoBehaviour
+{
+    public GameObject Texte;
+    public GameObject Aide;
+
+    public void Afficher()
+    {
+        Aide.gameObject.SetActive(false);
+        Texte.gameObject.SetActive(true);
+    }
+
+    public void Cacher()
+    {
+        Aide.gameObject.SetActive(true);
+        Texte.gameObject.SetActive(false);
+    }
+}
