8ba709771a338cf6439bb2418f0cab20b32f1d2d
AdrienBotrel <adrien.botrel1@uqac.ca>
2021-11-20 17:47:19 -0500

    Création d'animation lorsqu'un bonus est activé

--- UQAC_Game/Assets/Scripts/UI/BonusPanel.cs
+++ UQAC_Game/Assets/Scripts/UI/BonusPanel.cs
@@ -1,18 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class BonusPanel : MonoBehaviour
 {
+    public float cooldownMax;
+    public float currentCooldown;
+    private int bonusNumber;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cooldownMax = 1.2f;
+        if (gameObject.name == "Bonus 1")
+        {
+            bonusNumber = 1;
+        }
+        else if (gameObject.name == "Bonus 2")
+        {
+            bonusNumber = 2;
+        }
+        else 
+        {
+            bonusNumber = 3;
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (bonusNumber == 1)
+        {
+            ActivateBonus(Input.GetKeyDown(KeyCode.Alpha1));
+        }
+        else if (bonusNumber == 2)
+        {
+            ActivateBonus(Input.GetKeyDown(KeyCode.Alpha2));
+        }
+        else
+        {
+            ActivateBonus(Input.GetKeyDown(KeyCode.Alpha3));
+        }
+
         
     }
+
+    private void ActivateBonus(bool keyCode)
+    {
+        if (keyCode && gameObject.GetComponent<Image>().fillAmount == 1)
+        {
+            gameObject.GetComponent<Image>().fillAmount = 0;
+            gameObject.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().gameObject.SetActive(false);
+            currentCooldown = 0;
+        }
+
+        if (gameObject.GetComponent<Image>().fillAmount != 1)
+        {
+            currentCooldown += Time.deltaTime;
+            if (currentCooldown >= cooldownMax)
+            {
+                gameObject.transform.GetChild(1).gameObject.GetComponent<TextMeshProUGUI>().gameObject.SetActive(true);
+                currentCooldown = cooldownMax;
+            }
+            gameObject.GetComponent<Image>().fillAmount = currentCooldown / cooldownMax;
+            gameObject.transform.GetChild(0).gameObject.GetComponent<Image>().fillAmount = currentCooldown / cooldownMax;
+        }
+    }
+
 }
