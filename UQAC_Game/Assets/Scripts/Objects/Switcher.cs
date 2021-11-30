b8af65045d64f800eb7a533772715af84b52d527
M4ti5 <Caffiauxmatis@gmail.com>
2021-11-30 02:58:18 +0100

    Switcher Object

--- UQAC_Game/Assets/Scripts/Objects/Switcher.cs
+++ UQAC_Game/Assets/Scripts/Objects/Switcher.cs
@@ -15,24 +15,40 @@ public class Switcher : Object {
 
             //Player random
             int randomIndex = Random.Range(0 , listPlayers.childCount-1);
-            Transform randomPlayer = listPlayers.GetChild(randomIndex) == GetComponent<PhotonView>().IsMine ? listPlayers.GetChild(randomIndex+1 % listPlayers.childCount-1) : listPlayers.GetChild(randomIndex);
+            Transform randomPlayer = listPlayers.GetChild(randomIndex) == GetComponent<PhotonView>().IsMine ? listPlayers.GetChild(randomIndex+1 % listPlayers.childCount) : listPlayers.GetChild(randomIndex);
 
             //Local task
             Switch(randomPlayer);
             print(randomPlayer.gameObject.GetPhotonView().ViewID);
-            print(PhotonNetwork.PlayerList[0].UserId);
+            print(PhotonNetwork.PlayerList[0].ActorNumber);
 
-            Photon.Realtime.Player networkBindRandomPlayer = PhotonNetwork.PlayerList.Where(player => player.UserId == randomPlayer.gameObject.GetPhotonView().ViewID.ToString()).First();
+            Photon.Realtime.Player networkBindRandomPlayer = PhotonNetwork.PlayerList.Where(player => player.ActorNumber == randomPlayer.gameObject.GetPhotonView().ControllerActorNr).First();
 
             //Network task
-            photonView.RPC(nameof(Switch) , networkBindRandomPlayer , player); ;
+            photonView.RPC(nameof(Switch) , networkBindRandomPlayer , player , randomPlayer.gameObject.GetPhotonView().ViewID ); ;
            
         }
+    }
+
+    Transform FindPlayerByID (int id) {
+        foreach (Transform child in allPlayers.transform) {
+            if (child.GetComponent<PhotonView>().ViewID == id) {
+                return child;
+            }
+        }
+        return null;
     }
 
     [PunRPC]
     private void Switch (Transform other) {
         transform.parent.parent.position = other.position;
         transform.parent.parent.rotation = other.rotation;
+    }
+
+    [PunRPC]
+    private void Switch (Transform other , int otherId) {
+        Transform temp = FindPlayerByID(otherId);
+        temp.position = other.position;
+        temp.rotation = other.rotation;
     }
 }
