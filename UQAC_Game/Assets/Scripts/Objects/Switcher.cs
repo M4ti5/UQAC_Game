1a317ec08fb3f7a6ae6a7a725c9c2827a4baa075
AntTBD <antbd@free.fr>
2021-11-30 09:41:25 -0500

    fix use my object + wait before destroy player

--- UQAC_Game/Assets/Scripts/Objects/Switcher.cs
+++ UQAC_Game/Assets/Scripts/Objects/Switcher.cs
@@ -7,25 +7,35 @@ using System.Linq;
 public class Switcher : Object {
 
     public override void Behaviour () {
-        if (Time.time - lastTimeUseObject > deltaTimeUseObject) {
-            lastTimeUseObject = Time.time;
 
-            Transform player = transform.parent.parent;
-            Transform listPlayers = player.parent;
+        if (player.GetComponent<PhotonView>().IsMine == true)
+        {
+            if (Time.time - lastTimeUseObject > deltaTimeUseObject)
+            {
+                lastTimeUseObject = Time.time;
 
-            //Player random
-            int randomIndex = Random.Range(0 , listPlayers.childCount-1);
-            Transform randomPlayer = listPlayers.GetChild(randomIndex) == GetComponent<PhotonView>().IsMine ? listPlayers.GetChild(randomIndex+1 % listPlayers.childCount) : listPlayers.GetChild(randomIndex);
+                Transform player = transform.parent.parent;
+                Transform listPlayers = player.parent;
 
-            //Network task
-            Photon.Realtime.Player networkBindRandomPlayer = PhotonNetwork.PlayerList.Where(player => player.ActorNumber == randomPlayer.gameObject.GetPhotonView().ControllerActorNr).First();
-            photonView.RPC(nameof(Switch) , networkBindRandomPlayer , player.position , player.rotation, randomPlayer.gameObject.GetPhotonView().ViewID ); ;
+                //Player random
+                int randomIndex = Random.Range(0, listPlayers.childCount - 1);
+                Transform randomPlayer = listPlayers.GetChild(randomIndex) == GetComponent<PhotonView>().IsMine
+                    ? listPlayers.GetChild(randomIndex + 1 % listPlayers.childCount)
+                    : listPlayers.GetChild(randomIndex);
+
+                //Network task
+                Photon.Realtime.Player networkBindRandomPlayer = PhotonNetwork.PlayerList.Where(player =>
+                    player.ActorNumber == randomPlayer.gameObject.GetPhotonView().ControllerActorNr).First();
+                photonView.RPC(nameof(Switch), networkBindRandomPlayer, player.position, player.rotation,
+                    randomPlayer.gameObject.GetPhotonView().ViewID);
+                ;
 
 
-            //Local task
-            Switch(randomPlayer.position , randomPlayer.rotation);
-            print(randomPlayer.gameObject.GetPhotonView().ViewID);
-            print(PhotonNetwork.PlayerList[0].ActorNumber);
+                //Local task
+                Switch(randomPlayer.position, randomPlayer.rotation);
+                print(randomPlayer.gameObject.GetPhotonView().ViewID);
+                print(PhotonNetwork.PlayerList[0].ActorNumber);
+            }
         }
     }
 
