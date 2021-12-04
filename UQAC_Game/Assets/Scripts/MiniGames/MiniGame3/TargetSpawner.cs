f9c29497d853be75f727f9704b0468d449b5bd42
Paul <paul.delarue@student.junia.com>
2021-12-04 11:55:59 -0500

    fix target positions in minigame
    target position are set so that it is in canvas

--- UQAC_Game/Assets/Scripts/MiniGames/MiniGame3/TargetSpawner.cs
+++ UQAC_Game/Assets/Scripts/MiniGames/MiniGame3/TargetSpawner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class TargetSpawner : MonoBehaviour
 {
     public int nbOfTarget = 1;
@@ -10,6 +11,8 @@ public class TargetSpawner : MonoBehaviour
     public GameObject victoryText;
     private float maxWidth;
     private float maxHeight;
+
+    private float sizeOfTargetSprite = 100;
     
     public List<GameObject> allTargets;
 
@@ -40,8 +43,8 @@ public class TargetSpawner : MonoBehaviour
         {
             GameObject tmpObj = Instantiate(targetPrefab);
             tmpObj.transform.SetParent(transform, false);
-            float x = Random.Range(-maxWidth/2, maxWidth/2);
-            float y = Random.Range(-maxHeight/2, maxHeight/2);
+            float x = Random.Range(-maxWidth/2+sizeOfTargetSprite/2, maxWidth/2-sizeOfTargetSprite/2);
+            float y = Random.Range(-maxHeight/2+sizeOfTargetSprite/2, maxHeight/2-sizeOfTargetSprite/2);
             tmpObj.transform.localPosition = new Vector3(x, y, 1);
             tmpObj.transform.localScale = new Vector3(1, 1, 1);
             tmpObj.transform.localRotation = Quaternion.identity;
