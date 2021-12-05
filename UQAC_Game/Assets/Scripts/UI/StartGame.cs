8d6b5aaef2e1500d3020799a59432c40f878bd53
AdrienBotrel <adrien.botrel1@uqac.ca>
2021-12-04 23:18:09 -0500

    Gestion du timer du startGame en public + désactivation du canvas au début

--- UQAC_Game/Assets/Scripts/UI/StartGame.cs
+++ UQAC_Game/Assets/Scripts/UI/StartGame.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class StartGame : MonoBehaviour
 {
+    public float waitingTime = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.SetActive(true);
+        transform.GetChild(0).gameObject.SetActive(true);
         StartCoroutine(DesactiveImageStartGame());
     }
 
     IEnumerator DesactiveImageStartGame()
     {
-        yield return new WaitForSeconds(2f);
-        gameObject.SetActive(false);
+        yield return new WaitForSeconds(waitingTime);
+        transform.GetChild(0).gameObject.SetActive(false);
     }
 }
