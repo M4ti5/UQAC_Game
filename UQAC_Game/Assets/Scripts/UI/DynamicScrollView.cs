0e337d8fed34c84c7a72a5e3913907c783a92073
AntTBD <antbd@free.fr>
2021-12-07 21:37:39 -0500

    upgrade aide message to use scroll bar if too height
    + change scene text

--- UQAC_Game/Assets/Scripts/UI/DynamicScrollView.cs
+++ UQAC_Game/Assets/Scripts/UI/DynamicScrollView.cs
@@ -12,31 +12,17 @@ public class DynamicScrollView : MonoBehaviour
     public TextMeshProUGUI textMeshPro;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        print("textMeshPro.fontSize :"+textMeshPro.fontSize +"\n"+
-              "textMeshPro.textInfo.lineCount :"+textMeshPro.textInfo.lineCount +"\n"+
-              "content.sizeDelta.x :"+content.sizeDelta.x +"\n"+
-              "content.sizeDelta.y :"+content.sizeDelta.y +"\n"+
-              "-textMeshPro.GetComponent<RectTransform>().offsetMax.y :"+-textMeshPro.GetComponent<RectTransform>().offsetMax.y +"\n"+
-              "textMeshPro.GetComponent<RectTransform>().offsetMin.y :"+textMeshPro.GetComponent<RectTransform>().offsetMin.y +"\n"
-              );
-    }
-
     // Update is called once per frame
     void Update()
     {
-        content.sizeDelta = new Vector2(content.sizeDelta.x, 
-            textMeshPro.fontSize * textMeshPro.textInfo.lineCount + 
-            (-textMeshPro.GetComponent<RectTransform>().offsetMax.y) + // top
-            textMeshPro.GetComponent<RectTransform>().offsetMin.y); // bottom
-        /*float sizeViewPort =
-            content.rect.height + horizontalScrollBar.rect.height;
-        sizeViewPort > 
-        viewPort.SetSizeWithCurrentAnchors(
-            RectTransform.Axis.Vertical,
-            sizeViewPort
-        );*/
+        float height = textMeshPro.preferredHeight +
+                       (-textMeshPro.GetComponent<RectTransform>().offsetMax.y) + // top
+                       textMeshPro.GetComponent<RectTransform>().offsetMin.y; // bottom
+        if (height < content.parent.GetComponent<RectTransform>().rect.height)
+        {
+            height = content.parent.GetComponent<RectTransform>().rect.height;
+        }
+        
+        content.sizeDelta = new Vector2(content.sizeDelta.x, height);
     }
 }
