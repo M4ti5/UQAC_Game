104d21bdde2e5897ed3faa1e2c9a76df9f329029
AntTBD <antbd@free.fr>
2021-12-07 23:22:46 -0500

    disable move at start

--- UQAC_Game/Assets/Scripts/UI/StartGame.cs
+++ UQAC_Game/Assets/Scripts/UI/StartGame.cs
@@ -1,21 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class StartGame : MonoBehaviour
 {
     public float waitingTime = 5;
+    public Transform allPlayers;
 
     // Start is called before the first frame update
     void Start()
     {
+        if (allPlayers == null)
+            allPlayers = GameObject.Find("Players").transform;
         transform.GetChild(0).gameObject.SetActive(true);
         StartCoroutine(DesactiveImageStartGame());
     }
 
     IEnumerator DesactiveImageStartGame()
     {
+        yield return toggleMoveMyPlayer(false);
+        
         yield return new WaitForSeconds(waitingTime);
         transform.GetChild(0).gameObject.SetActive(false);
+        
+        yield return toggleMoveMyPlayer(true);
+    }
+
+    IEnumerator toggleMoveMyPlayer(bool canMove)
+    {
+        yield return new WaitWhile(() => allPlayers.childCount == 0);
+        foreach (Transform player in allPlayers)
+        {
+            if (player.GetComponent<PhotonView>().IsMine)
+                player.GetComponent<PlayerStatManager>().canMove = canMove;
+        }
     }
 }
